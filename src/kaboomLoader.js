ee88437b59fc1b0b2c96143c4515ecdfb52c999b
Roman Soltan <romabandit88@gmail.com>
2025-11-12 23:02:48 +0200

    implementing-drone-enemy

--- src/kaboomLoader.js
+++ src/kaboomLoader.js
@@ -1,4 +1,4 @@
-import kaboom from '../lib/kaboom.mjs';
+import kaboom from "../lib/kaboom.mjs";
 
 export const scale = 2;
 export const k = kaboom({
@@ -9,9 +9,9 @@ export const k = kaboom({
   global: false,
 });
 
-k.loadFont('glyphmesss', './assets/glyphmesss.ttf');
+k.loadFont("glyphmesss", "./assets/glyphmesss.ttf");
 
-k.loadSprite('player', './assets/sprites/u.png', {
+k.loadSprite("player", "./assets/sprites/u.png", {
   sliceX: 8,
   sliceY: 9,
   anims: {
@@ -24,7 +24,7 @@ k.loadSprite('player', './assets/sprites/u.png', {
   },
 });
 
-k.loadSprite('drone', './assets/sprites/dr0ne.png', {
+k.loadSprite("drone", "./assets/sprites/dr0ne.png", {
   sliceX: 6,
   sliceY: 3,
   anims: {
@@ -34,20 +34,20 @@ k.loadSprite('drone', './assets/sprites/dr0ne.png', {
   },
 });
 
-k.loadSprite('burner', './assets/sprites/burn3r.png', {
+k.loadSprite("burner", "./assets/sprites/burn3r.png", {
   sliceX: 5,
   sliceY: 6,
   anims: {
     idle: { from: 0, to: 3, loop: true },
     run: { from: 6, to: 8, loop: true },
-    'open-fire': { from: 10, to: 14 },
+    "open-fire": { from: 10, to: 14 },
     fire: { from: 15, to: 18, loop: true },
-    'shut-fire': { from: 20, to: 23 },
+    "shut-fire": { from: 20, to: 23 },
     explode: { from: 25, to: 29 },
   },
 });
 
-k.loadSpriteAtlas('./assets/ui.png', {
+k.loadSpriteAtlas("./assets/ui.png", {
   healthBar: {
     x: 16,
     y: 16,
@@ -57,7 +57,7 @@ k.loadSpriteAtlas('./assets/ui.png', {
   },
 });
 
-k.loadSpriteAtlas('./assets/animations.png', {
+k.loadSpriteAtlas("./assets/animations.png", {
   cartridge: {
     x: 125,
     y: 145,
@@ -70,20 +70,20 @@ k.loadSpriteAtlas('./assets/animations.png', {
   },
 });
 
-k.loadSprite('tileset', './assets/tileset.png', {
+k.loadSprite("tileset", "./assets/tileset.png", {
   sliceX: 33,
   sliceY: 21,
 });
 
-k.loadSprite('background', './assets/background.png', {
+k.loadSprite("background", "./assets/background.png", {
   sliceX: 13,
   sliceY: 25,
 });
 
-k.loadSound('notify', './assets/sounds/notify.mp3');
-k.loadSound('boom', './assets/sounds/boom.wav');
-k.loadSound('health', './assets/sounds/health.wav');
-k.loadSound('flamethrower', './assets/sounds/flamethrower.mp3');
+k.loadSound("notify", "./assets/sounds/notify.mp3");
+k.loadSound("boom", "./assets/sounds/boom.wav");
+k.loadSound("health", "./assets/sounds/health.wav");
+k.loadSound("flamethrower", "./assets/sounds/flamethrower.mp3");
 
-k.loadSprite('room1', './maps/room1.png');
-k.loadSprite('room2', './maps/room2.png');
+k.loadSprite("room1", "./maps/room1.png");
+k.loadSprite("room2", "./maps/room2.png");
